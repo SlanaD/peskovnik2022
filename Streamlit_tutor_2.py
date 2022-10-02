60770e7cbf7a73035e9c369051cb06c2b581ac9e
SlanaD <Slana.Danilo@gmail.com>
2022-10-02 19:55:40 +0200

    Update Streamlit_tutor_2.py

--- Streamlit_tutor_2.py
+++ Streamlit_tutor_2.py
@@ -1,41 +1,76 @@
-#https://docs.streamlit.io/library/get-started/create-an-app
-#py -m streamlit run Streamlit_tutor_2.py
-
+## SLD 2022-10-02
+# py -m streamlit run Dataframe_compare_streamlit.py
 
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+def highlight_diff(data, color='yellow'):
+    attr = 'background-color: {}'.format(color)
+    other = data.xs('self', axis='columns', level=-1)
+    return pd.DataFrame(np.where(data.ne(other, level=0), attr, ''),
+                        index=data.index, columns=data.columns)
+    
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+st.title('Primerjaj dva mesečna izvoza')
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
+#path_xlsx_A = r"C:\Users\slanad\OneDrive\Dokumenti\Palfinger\DATA\Tests\2022-07.xlsx"
+#path_xlsx_B = r"C:\Users\slanad\OneDrive\Dokumenti\Palfinger\DATA\Tests\2022-09.xlsx"
+sheet_name_A = "DATA2"
+sheet_name_B = "DATA2"
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+path_xlsx_A = st.file_uploader("Izberi mesec A:")
+if path_xlsx_A is not None:
+     ##read xlsx
+     dfA = pd.read_excel(path_xlsx_A, sheet_name=sheet_name_A, header=0,).iloc[:,0:7]
+#     st.dataframe(df1)
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+path_xlsx_B = st.file_uploader("Izberi mesec B:")
+if path_xlsx_B is not None:
+     ##read xlsx
+     dfB = pd.read_excel(path_xlsx_B, sheet_name=sheet_name_B, header=0,).iloc[:,0:7]
+#     st.dataframe(df1)
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+if st.button('PREVERI'):
+    with st.spinner("Training ongoing"):
+
+
+        #Novi sodelavci
+        dft=pd.merge(dfB,dfA,on=['MS'],how="outer",indicator=True)
+        dft=dft[dft['_merge']=='left_only'].iloc[:,1:6]
+
+        st.subheader(f"Novi sodelavci: {len(dft)}")
+        st.dataframe(dft)   
+
+        #Niso več v seznamu
+        dft=pd.merge(dfA,dfB,on=['MS'],how="outer",indicator=True)
+        dft=dft[dft['_merge']=='left_only'].iloc[:,1:6]
+
+        st.subheader(f"Niso več na seznemu: {len(dft)}")
+        st.dataframe(dft)  
+
+
+        vse_MS = (sorted( set(dfA["MS"]).union( set(dfB["MS"]) ) )) #tukaj so vse maticne iz obeh datotek
+        dfTemp = pd.DataFrame(list(vse_MS),columns=["MS_all"])
+        dfAA = dfTemp.merge(dfA, how='outer', left_on="MS_all", right_on="MS")
+        dfBB = dfTemp.merge(dfB, how='outer', left_on="MS_all", right_on="MS")
+
+        #in A is old in B is new - this is an agreement (). B=self, A=other
+
+        dfCC = dfBB.compare(dfAA, keep_equal=True, keep_shape=False)
+
+        st.subheader(f"Ostale spremembe:")
+        #st.text("pritisni check.box za prikaz")
+        #checkbox
+        #Spremembe pri tistih, ki so ostali
+        dft = dfCC[dfCC.MS.self.notnull() & dfCC.MS.other.notnull()==True]
+        #if st.checkbox('Spremembe pri ne-novih novih'):
+        st.table(dft.style.apply(highlight_diff, axis=None))
+
+        #Vse spremembe
+        #if st.checkbox('Pokaži vse spremembe'):
+        #st.dataframe(dfCC.style.apply(highlight_diff, axis=None))
+            
 
